076ae005ab43153b86f66ec3e44acef8a48ed04e
Fernando <garri.jf@gmail.com>
2025-09-16 14:05:27 +0200

    actualizació con nuevos archivos 16092025

--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -1,22 +1,54 @@
-import React from "react";
+import React, { Component } from "react";
 
 import { Link } from "react-router-dom";
 
-export default function(props) {
+export default class PortfolioItem extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            portfolioItemClass: ""
+        };
+    }
+
+    handleMouseEnter() {
+        this.setState({ portfolioItemClass: "image-blur" });
+    }
+
+    handleMouseLeave() {
+        this.setState({ portfolioItemClass: "" });
+    }
     //Datos que necesitamos:
         // -Imagen de fondo (Background): thumb_image_url
         // -Logo: logo_url
         // -Descripción del elemento del portafolios: description
         // -id (identificación): id
         // "id", "name", "description", "url", "category", "position", "thumb_image_url", "banner_image_url", "logo_url", "column_names_merged_with_images"
+    render() {
+        const { id, description, thumb_image_url, logo_url } = this.props.item;   
+        return (
+            <div 
+            className="portfolio-item-wrapper"
+            onMouseEnter={() => this.handleMouseEnter()}
+            onMouseLeave={() => this.handleMouseLeave()}
+            >
+                <div 
+                    className={
+                        "portfolio-img-background " + this.state.portfolioItemClass
+                    }
+                    style={{
+                        backgroundImage: "url(" + thumb_image_url + ")"
+                    }}
+                />    
+           
+                <div className="img-text-wrapper">
+                    <div className="logo-wrapper">
+                        <img src={logo_url} />
+                    </div>
 
-        const { id, description, thumb_image_url, logo_url} = props.item;   
-    return (
-        <div className="portfolio-item-wrapper">
-            <img src={thumb_image_url} />
-            <img src={logo_url} />
-            <div>{description}</div>
-            <Link to={`/portfolio/${id}`}>Link</Link>
-        </div>
-    );
+                    <div className="subtitle">¨{description}</div>
+                </div>
+            </div>
+        );
+    }
 }
