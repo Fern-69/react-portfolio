678bab1cd6550ffa3c5ad7cca95c9e633a000a85
Fernando <garri.jf@gmail.com>
2025-09-19 12:53:13 +0200

    Actualización hasta checkpoint 15

--- src/components/app.js
+++ src/components/app.js
@@ -13,12 +13,11 @@ import About from "./pages/about";
 import Contact from "./pages/contact";
 import Blog from "./pages/blog";
 import PortfolioDetail from "./portfolio/portfolio-detail"
+import Auth from "./pages/auth";
 import NoMatch from "./pages/no-match";
 
 export default class App extends Component {
-  
   render() {
-
     return (
       <div className='container'>
         <Router>
@@ -33,6 +32,7 @@ export default class App extends Component {
 
               <Switch>
                 <Route exact path="/" component={Home} />
+                <Route path="/auth" component={Auth} />
                 <Route path="/about-me" component={About} />
                 <Route path="/contact" component={Contact} />
                 <Route path="/blog" component={Blog} />
